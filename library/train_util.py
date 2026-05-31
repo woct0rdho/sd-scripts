25119711669f1c694e4a62050391ff0dbcc4a83e
woctordho <woctordho@outlook.com>
2026-05-31 19:18:26 +0800

    Fix conditioning image cropping to be in pixel perfect alignment with the input image

--- library/train_util.py
+++ library/train_util.py
@@ -2651,21 +2651,13 @@ class ControlNetDataset(BaseDataset):
                     cond_img.shape[0] == original_size_hw[0] and cond_img.shape[1] == original_size_hw[1]
                 ), f"size of conditioning image is not match / 画像サイズが合いません: {image_info.absolute_path}"
 
-                cond_img = resize_image(
+                cond_img, _, _ = trim_and_resize_if_required(
+                    False,  # TODO support random crop
                     cond_img,
-                    original_size_hw[1],
-                    original_size_hw[0],
-                    target_size_hw[1],
-                    target_size_hw[0],
-                    self.resize_interpolation,
+                    image_info.bucket_reso,
+                    image_info.resized_size,
+                    resize_interpolation=image_info.resize_interpolation,
                 )
-
-                # TODO support random crop
-                # 現在サポートしているcropはrandomではなく中央のみ
-                h, w = target_size_hw
-                ct = (cond_img.shape[0] - h) // 2
-                cl = (cond_img.shape[1] - w) // 2
-                cond_img = cond_img[ct : ct + h, cl : cl + w]
             else:
                 # assert (
                 #     cond_img.shape[0] == self.height and cond_img.shape[1] == self.width
